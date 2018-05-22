1b3af584812c338b4a2666d624ce30b926fa5196
caiziruo <caiziruo@caiziruos-MacBook-Pro.local>
2018-05-22 19:32:03 +0800

    update code

--- Naive_denoising.py
+++ Naive_denoising.py
@@ -57,6 +57,8 @@ class Naive_denoising(object):
 		elif (prior == "pV"):
 			self.Lambda = Lambda
 			self.p = p
+		elif (prior == "phi_Laplace"):
+			self.Lambda = Lambda
 
 	# Definitions of the norms and the potential function.
 	def Phi(self, u): 
@@ -72,6 +74,7 @@ class Naive_denoising(object):
 	def Gaussian_minimizer(self, u): return self.Phi(u) + self.Gaussian_norm(u)
 	def TG_minimizer(self, u): return self.Phi(u) + self.Gaussian_norm(u) + self.TV_norm(u)
 	def pV_minimizer(self, u): return self.Phi(u) + self.pV_norm(u)
+	def phi_minimizer(self, u): return self.Phi(u) + self.Lambda * abs(2 - self.TV_norm(u))
 
 	# Maximum a Posterior. Computed by optimization package(scipy).
 	def Get_MAP(self): 
@@ -88,6 +91,9 @@ class Naive_denoising(object):
 		elif self.Prior == "pV":
 			res = minimize(self.pV_minimizer, input, method = 'L-BFGS-B', tol = 1e-6)
 			print("Loss:", self.Phi(res.x), self.pV_norm(res.x))
+		elif self.Prior == "phi_Laplace":
+			res = minimize(self.phi_minimizer, input, method = 'L-BFGS-B', tol = 1e-6)
+			print("Loss:", self.Phi(res.x), self.phi_minimizer(res.x) - self.Phi(res.x))
 
 		self.MAP = res.x
 		if res.success: print("Successfully get MAP from " + self.Prior + " prior.")
@@ -167,6 +173,10 @@ class Naive_denoising(object):
 			u_mean += u_samples[-1]
 			if i % (sample_size / 10) == 0: print((i + 0.0) / sample_size)
 
+			if i > 0 and i % (sample_size / 10) == 0:
+				self.Posterior_Mean = u_mean / len(u_samples)
+				# self.Plot_CM(len(u_samples) - 1, beta)
+
 			u_current = u_samples[-1]
 			random_walk = np.random.multivariate_normal(np.zeros(self.dimension_of_observation), 
 				self.noise_variance * np.eye(self.dimension_of_observation))
@@ -191,19 +201,23 @@ class Naive_denoising(object):
 		return u_samples
 
 	def Plot_observation(self):
-		plt.plot(self.x_ordinate, self.observation, 'x')
-		plt.show()
+		plt.plot(self.x_ordinate, self.observation, 'x', label = "observation")
+		plt.plot(self.x_ordinate, Gu(self.x_ordinate), 'r', label = "u(t)")
+		plt.legend()
+		# plt.title("observation")
+		if (self.show_figure): plt.show()
+		if (self.save_figure): plt.savefig('Observation_'+datetime.now().strftime("_%H%M%S.%f")+'.pdf')
 		plt.close('all')
 
 	def Plot_MAP(self):
-		if (self.Prior == "TV"):
+		if (self.Prior == "TV") or (self.Prior == "phi_Laplace"):
 			legend_text = "Lambda = "+str(self.Lambda)
 		elif (self.Prior == "Gaussian"):
 			legend_text = "gamma = "+str(self.gamma)+"\nd = "+str(self.d)
 		elif (self.Prior == "TG"):
 			legend_text = "Lambda = "+str(self.Lambda)+"\ngamma = "+str(self.gamma)+"\nd = "+str(self.d)
 		elif (self.Prior == "pV"):
-			legend_text = "Lambda = "+str(self.Lambda)+"\np = "+str(self.p)
+			legend_text = "Lambda = "+str(self.Lambda)+"\np = "+str(self.p) 
 
 		if (self.show_observation): plt.plot(self.x_ordinate, self.observation, 'x')
 		plt.plot(self.x_ordinate, self.MAP, label = legend_text)
@@ -224,7 +238,7 @@ class Naive_denoising(object):
 		elif (self.Prior == "pV"):
 			legend_text = "Sp="+str(sample_size)+"\nbeta="+str(beta)+"\nLambda="+str(self.Lambda)+"\np="+str(self.p)
 
-		if (self.show_observation): plt.plot(self.x_ordinate, self.observation, 'r')
+		if (self.show_observation): plt.plot(self.x_ordinate, self.observation, 'x')
 		plt.plot(self.x_ordinate, self.Posterior_Mean, label = legend_text)
 		plt.legend()
 		plt.title(self.Prior + " Prior Posterior Mean")
@@ -235,7 +249,20 @@ class Naive_denoising(object):
 
 ###########################################################################################################
 if __name__ == '__main__':
-	# Denoising_example = Naive_denoising(dimension_of_observation = 23, noise_variance = 0.01, show_observation = True, show_figure = True, save_figure = False)
+	"""
+	Maximum A Posterior Experiments with different priors.
+	"""
+	# Denoising_example = Naive_denoising(dimension_of_observation = 23, noise_variance = 0.01, show_observation = True, show_figure = False, save_figure = True)
+	# Denoising_example.Plot_observation()
+	# Denoising_example.Set_Prior(prior = "pV", Lambda = 0.4, p = 2)
+	# Denoising_example.Get_MAP()
+	# Denoising_example.Metropolis_Hastings(sample_size = 100000, beta = 0.3)
+	# Denoising_example.Set_Prior(prior = "pV", Lambda = 2, p = 1.5)
+	# Denoising_example.Get_MAP()
+	# Denoising_example.Metropolis_Hastings(sample_size = 100000, beta = 0.3)
+	# Denoising_example.Set_Prior(prior = "pV", Lambda = 10, p = 1)
+	# Denoising_example.Get_MAP()
+	# Denoising_example.Metropolis_Hastings(sample_size = 100000, beta = 0.2)
 	# Denoising_example.Set_Prior(prior = "TV", Lambda = 50)
 	# Denoising_example.Get_MAP()
 	# Denoising_example.Set_Prior(prior = "Gaussian", gamma = 0.1, d = 0.04)
@@ -251,38 +278,71 @@ if __name__ == '__main__':
 	# for d in np.linspace(0.04, 0.4, 10):
 	# 	Denoising_example.Set_Prior(prior = "Gaussian", gamma = 0.1, d = d)
 	# 	Denoising_example.Get_MAP()
+	# Denoising_example.Set_Prior(prior = "phi_Laplace", Lambda = 10000)
+	# Denoising_example.Get_MAP()
 
+	"""
+
+	"""
 	# Sample_example = Naive_denoising(dimension_of_observation = 89, noise_variance = 0.001, show_observation = False, show_figure = True, save_figure = False)
-	# Sample_example.Set_Prior("TG", Lambda = 50, gamma = 0.1, d = 0.04)
-	# Sample_example.S_pCN(sample_size = 1000, splitting_number = 5, beta = 0.1)
-	# Sample_example.Set_Prior("TV", Lambda = 50)
-	# Sample_example.Metropolis_Hastings(sample_size = 1000, beta = 0.01)
-	# Sample_example = Naive_denoising(dimension_of_observation = 177, noise_variance = 0.001, show_observation = False, show_figure = True, save_figure = False)
-	# Sample_example.Set_Prior("TV", Lambda = 50)
-	# Sample_example.Metropolis_Hastings(sample_size = 1000, beta = 0.01)
-	# Sample_example = Naive_denoising(dimension_of_observation = 353, noise_variance = 0.001, show_observation = False, show_figure = True, save_figure = False)
+	# Sample_example.Set_Prior("TG", Lambda = 50, gamma = 1, d = 0.04)
+	# Sample_example.S_pCN(sample_size = 10, splitting_number = 5, beta = 0.1)
 	# Sample_example.Set_Prior("TV", Lambda = 50)
 	# Sample_example.Metropolis_Hastings(sample_size = 1000, beta = 0.01)
 
-	# Sample_example = Naive_denoising(dimension_of_observation = 353, noise_variance = 0.01, show_observation = True, show_figure = True, save_figure = False)
+
+	# Sample_example = Naive_denoising(dimension_of_observation = 89, noise_variance = 0.01, show_observation = True, show_figure = True, save_figure = False)
 	# Sample_example.Set_Prior(prior = "TV", Lambda = 50)
-	# Sample_example.Metropolis_Hastings(sample_size = 1000000, beta = 0.01)
-	# Sample_example.Set_Prior(prior = "Gaussian", gamma = 0.1, d = 0.2)
-	# Sample_example.pCN(sample_size = 5000000, beta = 0.003)
-	# Sample_example.Set_Prior(prior = "TG", Lambda = 50, gamma = 0.1, d = 0.04)
-	# Sample_example.pCN(sample_size = 5000000, beta = 0.003)
-	# Sample_example.Set_Prior(prior = "TG", Lambda = 50, gamma = 0.1, d = 0.04)
-	# Sample_example.S_pCN(sample_size = 1000, splitting_number = 5, beta = 0.01)
-	# Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.1)
 	# Sample_example.Metropolis_Hastings(sample_size = 1000000, beta = 0.03)
+	# Sample_example.Set_Prior(prior = "Gaussian", gamma = 0.1, d = 0.2)
+	# Sample_example.pCN(sample_size = 3000000, beta = 0.05)
+	# Sample_example.Set_Prior(prior = "TG", Lambda = 50, gamma = 0.1, d = 0.04)
+	# Sample_example.pCN(sample_size = 2000000, beta = 0.038)
+	# Sample_example.Set_Prior(prior = "TG", Lambda = 50, gamma = 0.1, d = 0.03)
+	# Sample_example.pCN(sample_size = 2000000, beta = 0.035)
+	# Sample_example.Set_Prior(prior = "TG", Lambda = 50, gamma = 0.1, d = 0.02)
+	# Sample_example.pCN(sample_size = 2000000, beta = 0.025)
+	# Sample_example.Set_Prior(prior = "TG", Lambda = 50, gamma = 0.1, d = 0.01)
+	# Sample_example.pCN(sample_size = 2000000, beta = 0.015)
+	# Sample_example.Set_Prior(prior = "TG", Lambda = 50, gamma = 0.1, d = 0.005)
+	# Sample_example.pCN(sample_size = 2000000, beta = 0.01)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 5000000, beta = 0.07)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 60, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 5000000, beta = 0.06)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 70, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 5000000, beta = 0.05)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 80, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 5000000, beta = 0.04)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 90, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 5000000, beta = 0.03)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 100, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 5000, beta = 0.03)
 
-	Sample_example = Naive_denoising(dimension_of_observation = 89, noise_variance = 0.01, show_observation = False, show_figure = False, save_figure = False)
-	Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.5)
-	Sample_example.Metropolis_Hastings(sample_size = 1000, beta = 0.03)
-	Sample_example = Naive_denoising(dimension_of_observation = 177, noise_variance = 0.01, show_observation = False, show_figure = False, save_figure = False)
-	Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.5)
-	Sample_example.Metropolis_Hastings(sample_size = 1000, beta = 0.03)
-	Sample_example = Naive_denoising(dimension_of_observation = 353, noise_variance = 0.01, show_observation = False, show_figure = False, save_figure = False)
-	Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.5)
-	Sample_example.Metropolis_Hastings(sample_size = 1000, beta = 0.01)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.9)
+	# Sample_example.Metropolis_Hastings(sample_size = 10000, beta = 0.035)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.8)
+	# Sample_example.Metropolis_Hastings(sample_size = 10000, beta = 0.04)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.7)
+	# Sample_example.Metropolis_Hastings(sample_size = 10000, beta = 0.045)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.6)
+	# Sample_example.Metropolis_Hastings(sample_size = 10000, beta = 0.055)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 50, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 100000, beta = 0.07)
 
+
+	"""
+	prior = pV, Posterior Mean with different dimensions.
+	"""
+	# Sample_example = Naive_denoising(dimension_of_observation = 89, noise_variance = 0.01, show_observation = True, show_figure = False, save_figure = True)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 150, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 1000000, beta = 0.015)
+	# Sample_example = Naive_denoising(dimension_of_observation = 177, noise_variance = 0.01, show_observation = True, show_figure = False, save_figure = True)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 220, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 2000000, beta = 0.009)
+	# Sample_example = Naive_denoising(dimension_of_observation = 353, noise_variance = 0.01, show_observation = True, show_figure = False, save_figure = True)
+	# Sample_example.Set_Prior(prior = "pV", Lambda = 500, p = 0.5)
+	# Sample_example.Metropolis_Hastings(sample_size = 8000000, beta = 0.003)
+
+	
+	
